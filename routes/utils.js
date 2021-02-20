c439e0cd4563bee6b2a4d25825273a77ccc474cc
Ynui <ynui12@gmail.com>
2021-02-20 13:12:43 +0200

    preformance measure

--- routes/utils.js
+++ routes/utils.js
@@ -9,20 +9,13 @@ exports.validateRequest = (req, res, next) => {
     return next()
 }
 
-const getDurationInMilliseconds = (start) => {
-    const NS_PER_SEC = 1e9
-    const NS_TO_MS = 1e6
-    const diff = process.hrtime(start)
-
-    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
-}
 
 exports.measureTime = (req, res, next) => {
     console.log(`${req.method} ${req.originalUrl} [STARTED]`)
-    const start = process.hrtime()
+    const start = Date.now()
     res.on('finish', () => {
-        const durationInMilliseconds = getDurationInMilliseconds(start)
-        console.log(`${req.method} ${req.originalUrl} [FINISHED] ${durationInMilliseconds.toLocaleString()} ms`)
+        const durationInMilliseconds = Date.now() - start
+        console.log(`${req.method} ${req.originalUrl} [FINISHED] ${durationInMilliseconds} ms`)
     })
     next()
 }
